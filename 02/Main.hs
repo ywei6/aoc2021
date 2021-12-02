cf87fb5c3d7c458f965d7539908677a777f4f140
Yu Wei <wei.yu@outlook.com>
2021-12-02 21:29:41 +0800

    Solved Part 2 of Day 2.

--- 02/Main.hs
+++ 02/Main.hs
@@ -16,15 +16,28 @@ parseLine l =
 inputFromFile :: FilePath -> IO [Command]
 inputFromFile filePath = map parseLine . lines <$> readFile filePath
 
-step :: (Int, Int) -> Command -> (Int, Int)
-step (h, d) (F, i) = (h + i, d)
-step (h, d) (D, i) = (h, d + i)
-step (h, d) (U, i) = (h, d - i)
+step2 :: ((Int, Int), Int) -> Command -> ((Int, Int), Int)
+step2 ((h, d), a) (F, i) = ((h + i, d + a * i), a)
+step2 (s, a) (D, i) = (s, a + i)
+step2 (s, a) (U, i) = (s, a - i)
+
+solve2 :: [Command] -> Int
+solve2 l =
+  let ((h, d), _) = foldl step2 ((0, 0), 0) l
+  in h * d
+
+step1 :: (Int, Int) -> Command -> (Int, Int)
+step1 (h, d) (F, i) = (h + i, d)
+step1 (h, d) (D, i) = (h, d + i)
+step1 (h, d) (U, i) = (h, d - i)
 
 solve1 :: [Command] -> Int
 solve1 l =
-  let (h, d) = foldl step (0, 0) l
+  let (h, d) = foldl step1 (0, 0) l
   in h * d
 
 main :: IO ()
-main = undefined
+main = do
+  i <- inputFromFile "input.txt"
+  print ("Part 1: " ++ show (solve1 i))
+  print ("Part 2: " ++ show (solve2 i))
